65db5fda71dff7ea8d5d35e52bb028e241903a4a
ductienmt <dinhtangductien26@gmail.com>
2024-09-25 13:34:22 +0700

    create project

---
+++ src/main/java/com/be_planfortrips/BePlanfortripsApplication.java
@@ -0,0 +1,13 @@
+package com.be_planfortrips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BePlanfortripsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BePlanfortripsApplication.class, args);
+    }
+
+}
