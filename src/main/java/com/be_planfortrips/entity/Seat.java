055d0abf66af3bcae0db6376dde85d593ea6b242
Đinh Tăng Đức Tiến <100287902+ductienmt@users.noreply.github.com>
2024-10-09 15:12:12 +0700

    cmt flight

--- src/main/java/com/be_planfortrips/entity/Seat.java
+++ src/main/java/com/be_planfortrips/entity/Seat.java
@@ -1,43 +1,43 @@
-package com.be_planfortrips.entity;
-
-import jakarta.persistence.*;
-import lombok.*;
-import lombok.experimental.FieldDefaults;
-
-import java.util.List;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity
-@Builder
-@FieldDefaults(level = AccessLevel.PRIVATE)
-@Table(name = "seats")
-public class Seat {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "seat_id", nullable = false)
-    Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "airplane_id")
-    Airplane airplane;
-
-    @Column(name = "seat_number", length = 10)
-    String seatNumber;
-
-
-    @Column(name = "status", columnDefinition = "status_seat")
-    Status status;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "flight_id")
-    Flight flight;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "class_id")
-    TicketClass ticketClass;
-
-    @OneToMany(mappedBy = "seat", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
-    List<Ticket> tickets;
-}
+//package com.be_planfortrips.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.*;
+//import lombok.experimental.FieldDefaults;
+//
+//import java.util.List;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Entity
+//@Builder
+//@FieldDefaults(level = AccessLevel.PRIVATE)
+//@Table(name = "seats")
+//public class Seat {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "seat_id", nullable = false)
+//    Long id;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "airplane_id")
+//    Airplane airplane;
+//
+//    @Column(name = "seat_number", length = 10)
+//    String seatNumber;
+//
+//
+//    @Column(name = "status", columnDefinition = "status_seat")
+//    Status status;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "flight_id")
+//    Flight flight;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "class_id")
+//    TicketClass ticketClass;
+//
+//    @OneToMany(mappedBy = "seat", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
+//    List<Ticket> tickets;
+//}
--- src/main/java/com/be_planfortrips/services/interfaces/IFlightService.java
+++ src/main/java/com/be_planfortrips/services/interfaces/IFlightService.java
@@ -1,12 +1,12 @@
-package com.be_planfortrips.services.interfaces;
-
-import com.be_planfortrips.dto.FlightDto;
-import com.be_planfortrips.entity.Flight;
-
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.util.List;
-
-public interface IFlightService {
-    List<FlightDto> getFlightsByCitiesAndDepartureTime(Timestamp departureTime, String departureCity, String arrivalCity);
-}
+//package com.be_planfortrips.services.interfaces;
+//
+//import com.be_planfortrips.dto.FlightDto;
+//import com.be_planfortrips.entity.Flight;
+//
+//import java.sql.Timestamp;
+//import java.time.LocalDateTime;
+//import java.util.List;
+//
+//public interface IFlightService {
+//    List<FlightDto> getFlightsByCitiesAndDepartureTime(Timestamp departureTime, String departureCity, String arrivalCity);
+//}
