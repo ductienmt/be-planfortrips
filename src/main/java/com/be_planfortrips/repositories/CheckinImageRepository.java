09ee6502397829fc4bbef9cd52d10f930889c4ca
hungdt1509 <111280504+HungDang123@users.noreply.github.com>
2024-11-08 22:13:31 +0700

    update

--- src/main/java/com/be_planfortrips/repositories/CheckinImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.be_planfortrips.repositories;
-
-import com.be_planfortrips.entity.CheckinImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CheckinImageRepository extends JpaRepository<CheckinImage, Integer> {
-}
