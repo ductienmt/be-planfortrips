a14f6e4c14b20b6986e56f238533623d5f048f41
hnhut <hnhut1200@gmail.com>
2024-11-12 22:45:38 +0700

    Fix bug 11-12-2024 (Zalo)

--- src/main/java/com/be_planfortrips/dto/FeedbackDto.java
+++ src/main/java/com/be_planfortrips/dto/FeedbackDto.java
@@ -1,15 +1,29 @@
 package com.be_planfortrips.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
+
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class FeedbackDto {
 
+     @NotBlank(message = "Content cannot be blank")
      String content;
+
+     @NotNull(message = "Rating is required")
+     @Min(value = 1, message = "Rating must be at least 1")
+     @Max(value = 5, message = "Rating must not exceed 5")
      Integer rating;
+
+     @NotNull(message = "User ID is required")
      Long userId;
+
+     @NotNull(message = "Account Enterprise ID is required")
      Long accountEnterpriseId;
 }
