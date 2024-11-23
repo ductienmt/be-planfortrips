608bb67fdf244fefd391b118326c92293df3e6cf
hungdt1509 <111280504+HungDang123@users.noreply.github.com>
2024-11-23 10:15:14 +0700

    Update ITourService.java

--- src/main/java/com/be_planfortrips/services/interfaces/ITourService.java
+++ src/main/java/com/be_planfortrips/services/interfaces/ITourService.java
@@ -16,13 +16,7 @@ public interface ITourService {
     Page<TourResponse> getActiveTours(PageRequest request, String title,Integer rating, List<String> tags);
     TourResponse getByTourId(Integer id) throws Exception;
     void deleteTourById(Integer id);
-<<<<<<< HEAD
     TourResponse uploadImage(Integer id, List<MultipartFile> files) throws Exception;
     TourResponse deleteImage(Integer id,  List<Integer> imageIds) throws Exception;
-
-=======
-
-    // Get Schedule Available
     List<TourScheduleResponse> getScheduleAvailable(LocalDateTime day, String cityId);
->>>>>>> f5ef60153cc1ac20fec948869a423d0b87824a0a
 }
