a23d8e72e623df2a13f2fedc88c7ed7209a4c523
Đinh Tăng Đức Tiến <100287902+ductienmt@users.noreply.github.com>
2024-11-26 00:42:25 +0700

    add [method upload image and get all city, get by area]

--- src/main/java/com/be_planfortrips/dto/AccountEnterpriseDto.java
+++ src/main/java/com/be_planfortrips/dto/AccountEnterpriseDto.java
@@ -9,42 +9,15 @@ import lombok.experimental.FieldDefaults;
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class AccountEnterpriseDto {
-
-//    @NotEmpty(message = "Username cannot be empty")
     @Size(min = 5, max = 50, message = "Username must be between 5 and 50 characters")
     String username;
-
-
-//    @NotEmpty(message = "Email cannot be empty")
     @Email(message = "Email should be valid")
     String email;
-
-//    @NotEmpty(message = "Enterprise name cannot be empty")
     String enterpriseName;
-
-//    @NotEmpty(message = "Representative name cannot be empty")
     String representative;
-
-//    @NotEmpty(message = "Tax code cannot be empty")
-//    @Pattern(regexp = "\\d{3}", message = "Tax code must be 10 digits")
     String taxCode;
-
-//    @NotEmpty(message = "Phone number cannot be empty")
-//    @Pattern(regexp = "\\d{10,15}", message = "Phone number must be between 10 and 15 digits")
     String phoneNumber;
-
-//    @NotEmpty(message = "Address cannot be empty")
     String address;
-
-//    @NotNull(message = "Status cannot be null")
-//    Boolean status = false;
-
-//    @NotNull(message = "Image ID cannot be null")
-//    Integer imageId;
-
-//    @NotEmpty(message = "City ID cannot be empty")
     String cityId;
-
-//    @NotNull(message = "Type Enterprise Detail ID cannot be null")
     Long typeEnterpriseDetailId;
 }
--- src/main/java/com/be_planfortrips/services/impl/CityArea.java
+++ src/main/java/com/be_planfortrips/services/impl/CityArea.java
@@ -1,21 +1,92 @@
 package com.be_planfortrips.services.impl;
 
 import com.be_planfortrips.entity.City;
+import com.be_planfortrips.entity.Image;
+import com.be_planfortrips.entity.User;
+import com.be_planfortrips.exceptions.AppException;
+import com.be_planfortrips.exceptions.ErrorType;
 import com.be_planfortrips.repositories.CityRepository;
+import com.be_planfortrips.repositories.ImageRepository;
 import com.be_planfortrips.services.interfaces.ICityService;
+import com.be_planfortrips.utils.Utils;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE,makeFinal = true)
 public class CityArea implements ICityService {
     CityRepository cityRepository;
+    CloudinaryService cloudinaryService;
+    private final ImageRepository imageRepository;
+
     @Override
     public List<City> getCityByAreaId(String areaId) {
         return cityRepository.findByArea_Id(areaId);
     }
+
+    @Override
+    public List<Map<String, Object>> getCitiesByAreaId(String id) {
+        List<City> cities = cityRepository.findByArea_Id(id);
+        List<Map<String, Object>> citiesArea = new ArrayList<>();
+        for (City city : cities) {
+            Map<String, Object> cityMap = new HashMap<>();
+            cityMap.put("id", city.getId());
+            cityMap.put("nameCity", city.getNameCity());
+            cityMap.put("description", city.getDescription());
+            citiesArea.add(cityMap);
+        }
+        return citiesArea;
+    }
+
+    @Override
+    public List<Map<String, Object>> getAllCity() {
+        List<City> cities = cityRepository.findAll();
+        List<Map<String, Object>> cityList = new ArrayList<>();
+        for (City city : cities) {
+            Map<String, Object> cityMap = new HashMap<>();
+            cityMap.put("id", city.getId());
+            cityMap.put("nameCity", city.getNameCity());
+            cityMap.put("description", city.getDescription());
+            cityList.add(cityMap);
+        }
+        return cityList;
+    }
+
+    @Override
+    public void uploadImage(MultipartFile file, String city_id) {
+        if (file == null || file.isEmpty()) {
+            throw new IllegalArgumentException("Vui lòng chọn ảnh hợp lệ");
+        }
+
+        City city = cityRepository.findById(city_id).orElseThrow(() -> new AppException(ErrorType.notFound, "Không tìm thấy thành phố"));
+
+        Utils.checkSize(file);
+
+        String imageUrl;
+        try {
+            Map<String, Object> uploadResult = cloudinaryService.uploadFile(file, "city_images");
+            imageUrl = uploadResult.get("url").toString();
+        } catch (IOException e) {
+            throw new AppException(ErrorType.internalServerError);
+        }
+
+        Image image = new Image();
+        image.setUrl(imageUrl);
+        imageRepository.saveAndFlush(image);
+
+        city.setImage(image);
+        cityRepository.saveAndFlush(city);
+    }
+
+
 }
